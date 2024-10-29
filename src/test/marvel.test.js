6748ccd7b64ef2f86756a83434cb1b582415daa6
Slimecaster <nd070503@gmail.com>
2024-10-29 10:36:22 +0100

    Test af GET-metode til at hente 1 helt ved brug af ID

--- src/test/marvel.test.js
+++ src/test/marvel.test.js
@@ -1,9 +1,8 @@
-const chai=require('chai');
-const chaiHttp=require('chai-http');
-const server=require('../../server');
-const should=chai.should();
+const { expect } = require('chai');
+const request = require('supertest')
+const app=require('../../server');
+const {description} = require("mocha/lib/cli/init");
 
-chai.use(chaiHttp);
 
 describe('Marvel helte API', ()=>{
     /**
@@ -11,7 +10,7 @@ describe('Marvel helte API', ()=>{
      */
     describe('GET /heroes', ()=>{
         it('should get all the heroes', (done)=>{
-            chai.request(server).get('/heroes').end((err,res) => {
+            chai.request(app).get('/heroes').end((err,res) => {
                 res.should.have.status(200);
                 res.body.should.be.a('array');
                 res.body.length.should.be.eql(5);
@@ -23,16 +22,25 @@ describe('Marvel helte API', ()=>{
     /**
      * Test GET-metoden for en enkelt helt ved brug af et givent id
      */
+
     describe('GET /heroes/:id', ()=>{
         it('should get a single hero by id', (done)=>{
-
+            chai.request(app).get('/heroes/:id').end((err,res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('id', id);
+                res.body.should.have.property('name', name);
+                res.body.should.have.property('alias', alias);
+                res.body.should.have.property('powers', powers);
+                done();
+            })
         })
     })
-
 
     /**
      * Test POST-metoden for en enkelt helt
      */
+
     describe()
 
 })
