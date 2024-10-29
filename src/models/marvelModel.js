c4a16ed4b115d6072235a571c637efdc4086026c
Slimecaster <nd070503@gmail.com>
2024-10-29 14:09:26 +0100

    Færdig 2.0

--- src/models/marvelModel.js
+++ src/models/marvelModel.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const filePath = path.join(__dirname, 'heroes.json');
+const filePath = path.join(__dirname, '../../heroes.json');
 
 // Function to read data from JSON file
 function readData() {
